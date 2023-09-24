1c1a1ff008b615ca772f43efa376cc7d7dfbbbc6
charlesstrauss <u1319464@umail.utah.edu>
2023-09-23 23:33:10 -0600

    Added run.sh for TA

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd HWs/HW_1/
+
+echo '###############'
+echo Running HW_1/2b
+echo '###############'
+python HW_1_part2_2b.py
+
+echo '###############'
+echo Running HW_1/3a
+echo '###############'
+python HW_1_part2_3a.py
+
+echo '###############'
+echo Running HW_1/3b
+echo '###############'
+python HW_1_part2_3b.py
+
