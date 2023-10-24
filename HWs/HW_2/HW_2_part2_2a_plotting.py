70b805d0965ce18380d5668472bded2dd1c6d84d
charlesstrauss <u1319464@umail.utah.edu>
2023-10-23 23:58:12 -0600

    results

--- HWs/HW_2/HW_2_part2_2a_plotting.py
+++ HWs/HW_2/HW_2_part2_2a_plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-results = pd.read_csv('../results/HW_2_part2_2a_errors_to_iterations.csv')
+results = pd.read_csv('./HWs/HW_2/results/HW_2_part2_2a_errors_to_iterations.csv')
 
 plt.plot(results.iterations, results.training_error/5000)
 plt.plot(results.iterations, results.training_error/5000, '.')
@@ -21,13 +21,13 @@ plt.title('Testing Errors vs. Number of Weak Classifiers in AdaBoost')
 plt.show()
 plt.close()
 
-classifier_errors = pd.read_csv('../results/HW_2_part2_2a_classifier_errors.csv')
+classifier_errors = pd.read_csv('./HWs/HW_2/results/HW_2_part2_2a_classifier_errors.csv')
 
 plt.plot(classifier_errors.classifier, classifier_errors.training_error/5000)
 # plt.plot(classifier_errors.classifier, classifier_errors.training_error/5000, '.')
 plt.xlabel('i')
 plt.ylabel('Errors on Training Set')
-plt.title('Training Error of Classifier i')
+plt.title('Training Error of Sub-Classifier i')
 plt.show()
 plt.close()
 
@@ -35,6 +35,6 @@ plt.plot(classifier_errors.classifier, classifier_errors.testing_error/5000)
 # plt.plot(classifier_errors.classifier, classifier_errors.testing_error/5000, '.')
 plt.xlabel('i')
 plt.ylabel('Errors on Testing Set')
-plt.title('Testing Error of Classifier i')
+plt.title('Testing Error of Sub-Classifier i')
 plt.show()
 plt.close()
